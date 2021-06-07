a30d12f606c160c8b38c5b43af58e85e5ab386ad
Rome Bell <romebell5@gmail.com>
2021-06-07 08:09:32 -0700

    users: add signup route

--- api/users.js
+++ api/users.js
@@ -15,12 +15,48 @@ const test = async (req, res) => {
     res.json({ message: 'User endpoint OK!'});
 }
 
+const signup = async (req, res) => {
+    console.log('--- INSIDE OF SIGNUP ---');
+    console.log('req.body =>', req.body);
+    const { name, email, password } = req.body;
+
+    try {
+        // see if a user exist in the database by email
+        const user = await User.findOne({ email });
+
+        // if a user exist return 400 error and message
+        if (user) {
+            return res.status(400).json({ message: 'Email already exists' });
+        } else {
+            console.log('Create new user');
+            let saltRounds = 12;
+            let salt = await bcrypt.genSalt(saltRounds);
+            let hash = await bcrypt.hash(password, salt);
+
+            const newUser = new User({
+                name,
+                email,
+                password: hash
+            });
+
+            const savedNewUser = await newUser.save();
+
+            res.json(savedNewUser);
+
+        }
+    } catch (error) {
+        console.log('Error inside of /api/users/signup');
+        console.log(error);
+        return res.status(400).json({ message: 'Error occurred. Please try again...'});
+    }
+}
+
 // routes
 // GET -> /api/users/test
 router.get('/test', test);
 
-// POST api/users/register (Public)
-// router.post('/signup', signup);
+// POST -> api/users/signup (Public)
+router.post('/signup', signup);
 
 // POST api/users/login (Public)
 // router.post('/login', login);
